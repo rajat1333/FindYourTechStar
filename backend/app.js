92c9a898389aa48cb79506b2c418340600b9d49e
utkarshpant112 <utkarshpant112@gmail.com>
2022-05-11 18:41:21 -0700

    proxy edit

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const validate = require("./util/authValidator");
 
 //  Importing routes
 const matchRoute = require("./src/routes/matchRoute");
-const messageRoute = require('./src/routes/messageRoute');
+const messageRoute = require("./src/routes/messageRoute");
 const signUpRoute = require("./src/routes/signUpRoute");
 const loginRoute = require("./src/routes/loginRoute");
 const userRoute = require("./src/routes/userRoute");
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(cors());
 // app.use(cors({ origin: '*', credentials: true }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://3.12.151.154:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -42,7 +42,7 @@ const validationMid = getAuthMiddleware(validate);
 
 //  API Endpoints
 app.use("/matches", matchRoute);
-app.use('/messages', messageRoute);
+app.use("/messages", messageRoute);
 app.use("/signUp", signUpRoute);
 app.use("/login", loginRoute);
 app.use("/users", userRoute);
